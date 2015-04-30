8de21ef3301366252daf8d0c366d1a0bc207df7d
liftting <458286096@qq.com>
2015-04-30 17:49:01 +0800

    init headview logic

--- XmAndroidCore/app/src/main/java/com/xm/cygcore/view/dataloadview/XmAutoLoadListView.java
+++ XmAndroidCore/app/src/main/java/com/xm/cygcore/view/dataloadview/XmAutoLoadListView.java
@@ -2,6 +2,8 @@ package com.xm.cygcore.view.dataloadview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewTreeObserver;
 import android.widget.AbsListView;
 import android.widget.ListView;
 
@@ -11,8 +13,11 @@ import android.widget.ListView;
 public class XmAutoLoadListView extends ListView implements AbsListView.OnScrollListener {
 
     private Context mContext;
-    private XmLoadFooterView mLoadFooterView;
+    //    private XmLoadFooterView mLoadFooterView;
     private XmLoadHeaderView mLoadHeaderView;
+    private int mHeadViewHeight;
+
+    private float mLastY;
 
     public XmAutoLoadListView(Context context) {
         super(context);
@@ -24,10 +29,60 @@ public class XmAutoLoadListView extends ListView implements AbsListView.OnScroll
 
     public XmAutoLoadListView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
     private void init(Context context) {
         mContext = context;
+        mLoadHeaderView = new XmLoadHeaderView(mContext);
+
+        addHeaderView(mLoadHeaderView);
+        mLoadHeaderView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mHeadViewHeight = mLoadHeaderView.getHeight();
+                getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float y = ev.getRawY();
+                float yDis = y - mLastY;
+                mLastY = y;
+
+                if (getFirstVisiblePosition() == 0 && yDis > 0) {
+                    updateHeadViewHeight((int) yDis);
+                }
+
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+        return super.onTouchEvent(ev);
+    }
+
+    private void updateHeadViewHeight(int dis) {
+        mLoadHeaderView.updateHeight(mLoadHeaderView.getVisibleHeight() + dis);
+
+        if (mLoadHeaderView.getVisibleHeight() >= mHeadViewHeight) {
+            // release to refresh
+            mLoadHeaderView.updateStateInfo(XmLoadHeaderView.STATE_PULLING);
+        } else {
+            mLoadHeaderView.updateStateInfo(XmLoadHeaderView.STATE_IDEL);
+        }
+
     }
 
     @Override
--- XmAndroidCore/app/src/main/java/com/xm/cygcore/view/dataloadview/XmLoadHeaderView.java
+++ XmAndroidCore/app/src/main/java/com/xm/cygcore/view/dataloadview/XmLoadHeaderView.java
@@ -2,18 +2,30 @@ package com.xm.cygcore.view.dataloadview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.xm.cygcore.R;
 
 /**
- * Created by wm on 15/4/27.
+ *
+ *
+ *
  */
 public class XmLoadHeaderView extends LinearLayout {
 
-    public final static int STATE_NORMAL = 0; //
+    public final static int STATE_IDEL = 0; //
     public final static int STATE_PULLING = 1; //
     public final static int STATE_REFRESH = 2; //
+    private RelativeLayout mContainer;
+    private TextView mTvTipView; // show status tip
+
 
     private Context mContext;
+    private int mCurrentState = STATE_IDEL;
 
     public XmLoadHeaderView(Context context) {
         super(context);
@@ -25,9 +37,46 @@ public class XmLoadHeaderView extends LinearLayout {
 
     public XmLoadHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
-    private void init(Context context){
+    private void init(Context context) {
+        mContext = context;
+        mContainer = (RelativeLayout) LayoutInflater.from(mContext).inflate(R.layout.layer_auto_load_header, null);
+        this.addView(mContainer, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0));
+        mTvTipView = (TextView) mContainer.findViewById(R.id.tv_auto_head_loading_status);
 
     }
+
+    public void updateStateInfo(int currentState) {
+        if (mCurrentState == currentState) return;
+
+        switch (currentState) {
+            case STATE_IDEL:
+                mTvTipView.setText("下拉刷新");
+                break;
+            case STATE_PULLING:
+                mTvTipView.setText("释放刷新");
+                break;
+            case STATE_REFRESH:
+                mTvTipView.setText("正在刷新");
+                break;
+        }
+        mCurrentState = currentState;
+
+    }
+
+    public void updateHeight(int height) {
+
+        if (height < 0) height = 0;
+
+        LinearLayout.LayoutParams params = (LayoutParams) mContainer.getLayoutParams();
+        params.height = height;
+        mContainer.setLayoutParams(params);
+    }
+
+    public int getVisibleHeight(){
+        return mContainer.getHeight();
+    }
+
 }
