6dad7198509c1689363861ac3bd35970baac5cdf
liftting <458286096@qq.com>
2015-05-04 18:47:42 +0800

    add plan package

--- XmAndroidCore/app/src/main/java/com/xm/cygcore/view/progressbar/CircleProgressBar.java
+++
@@ -1,7 +0,0 @@
-package com.xm.cygcore.view.progressbar;
-
-/**
- * Created by wm on 15/5/4.
- */
-public class CircleProgressBar {
-}
---
+++ XmAndroidCore/app/src/main/java/com/xm/cygcore/view/progressbar/CircleProgressDrawable.java
@@ -0,0 +1,32 @@
+package com.xm.cygcore.view.progressbar;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by wm on 15/5/4.
+ */
+public class CircleProgressDrawable extends Drawable{
+
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
